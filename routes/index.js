3dff5cb2c3c2f59ceb17292739471e06c390eccc
Michael Brownlow <michael.brownlow1999@gmail.com>
2019-12-10 13:42:46 -0500

    username display on login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var username; 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Woodstock Online' });
+  try{
+    username = req.user.username
+  }catch(exception){
+    username = ""
+  }
+  res.render('index', { title: 'Woodstock Online', username });
 });
 
 module.exports = router;
